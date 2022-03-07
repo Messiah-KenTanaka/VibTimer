bba6106311df8d34bde1a3fe63fed5a30a0fa731
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2022-03-07 12:21:50 +0900

    Initial commit

---
+++ app/src/main/java/com/beit_and_pear/vibtimer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.beit_and_pear.vibtimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
