41b680a687c7f45fb43a37f1c7c6b2715ef4e2b2
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2022-03-07 20:12:57 +0900

    MainActivity.ktを修正

--- app/src/main/java/com/beit_and_pear/vibtimer/MainActivity.kt
+++ app/src/main/java/com/beit_and_pear/vibtimer/MainActivity.kt
@@ -4,66 +4,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
-import android.widget.ImageButton
-import android.widget.TextView
-import com.beit_and_pear.vibtimer.MainActivity.Constants.Companion.START_TIMER
-import java.util.*
+import com.beit_and_pear.vibtimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    class Constants {
-        companion object {
-            const val START_TIMER = 1000
+    private lateinit var binding: ActivityMainBinding
+
+    inner class MyCountDownTimer(millisInFuture: Long, countDownInterval: Long)
+        : CountDownTimer(millisInFuture, countDownInterval) {
+
+        var isRunning = false
+
+        override fun onTick(millisUntilFinished: Long) {
+            val minute = millisUntilFinished / 1000L / 60L
+            val second = millisUntilFinished / 1000L % 60L
+            binding.textTimer.text = "%1d:%2$02d".format(minute, second)
+        }
+
+        override fun onFinish() {
+            binding.textTimer.text = "00:00"
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // ログ
         Log.i("MyActivity", "onCreate")
 
-        val textTimer = findViewById<TextView>(R.id.textTimer)
-        val imgBtnStart = findViewById<ImageButton>(R.id.imgBtnStart)
-        val imgBtnReset = findViewById<ImageButton>(R.id.imgBtnReset)
-        val timerRunning = false
+        binding.textTimer.text = "5:00"
+        var timer = MyCountDownTimer(5 * 60 * 1000, 100)
 
-        // スタートボタンを押したら
-        imgBtnStart.setOnClickListener {
-            println(timerRunning)
-            if (timerRunning) {
-                pauseTimer()
-            } else {
-                startTimer()
+        binding.imgBtnStart.setOnClickListener {
+            timer.isRunning = when (timer.isRunning) {
+                true -> {
+                    timer.cancel()
+                    binding.textTimer.text = "5:00"
+                    binding.imgBtnStart.setImageResource(
+                        R.drawable.btn_start)
+                    false
+                }
+                false -> {
+                    timer.start()
+                    binding.imgBtnStart.setImageResource(
+                        R.drawable.btn_reset)
+                    true
+                }
             }
         }
-
-        // リセットボタンを押したら
-        imgBtnReset.setOnClickListener {
-            resetTimer()
-        }
-    }
-
-    // タイマー実行
-    private fun startTimer() {
-
-
-    }
-    // 一時停止
-    private fun pauseTimer() {
-
-    }
-    // リセット
-    private fun resetTimer() {
-
-    }
-    // 時刻の表示
-    private fun updateCountDownText() {
-        var textView: TextView? = null
-        var minutes = (START_TIMER / 1000) / 60
-        var seconds = (START_TIMER / 1000) % 60
-        var timerFormatted = String.format(Locale.getDefault().toString(), "%02d:%02d", minutes, seconds)
-        textView?.text = timerFormatted
     }
 }
