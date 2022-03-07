6f69a4d7291724e4188dc6f9291a6185caf7ba24
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2022-03-07 19:14:16 +0900

    backgroundを設定

--- app/src/main/java/com/beit_and_pear/vibtimer/MainActivity.kt
+++ app/src/main/java/com/beit_and_pear/vibtimer/MainActivity.kt
@@ -2,9 +2,12 @@ package com.beit_and_pear.vibtimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.TextView
+import com.beit_and_pear.vibtimer.MainActivity.Constants.Companion.START_TIMER
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,20 +27,27 @@ class MainActivity : AppCompatActivity() {
         val textTimer = findViewById<TextView>(R.id.textTimer)
         val imgBtnStart = findViewById<ImageButton>(R.id.imgBtnStart)
         val imgBtnReset = findViewById<ImageButton>(R.id.imgBtnReset)
+        val timerRunning = false
 
         // スタートボタンを押したら
         imgBtnStart.setOnClickListener {
-
+            println(timerRunning)
+            if (timerRunning) {
+                pauseTimer()
+            } else {
+                startTimer()
+            }
         }
 
         // リセットボタンを押したら
         imgBtnReset.setOnClickListener {
-
+            resetTimer()
         }
     }
 
     // タイマー実行
     private fun startTimer() {
+
 
     }
     // 一時停止
@@ -49,7 +59,11 @@ class MainActivity : AppCompatActivity() {
 
     }
     // 時刻の表示
-    private fun update() {
-
+    private fun updateCountDownText() {
+        var textView: TextView? = null
+        var minutes = (START_TIMER / 1000) / 60
+        var seconds = (START_TIMER / 1000) % 60
+        var timerFormatted = String.format(Locale.getDefault().toString(), "%02d:%02d", minutes, seconds)
+        textView?.text = timerFormatted
     }
 }
